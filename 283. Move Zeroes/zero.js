54a29ddbf292810e0cb3ab447e4603fc94bd96ef
rishavtcz <rishav.kashyap@techchefz.com>
2025-02-12 14:51:15 +0530

    backend prep notes

--- 283. Move Zeroes/zero.js
+++ 283. Move Zeroes/zero.js
@@ -20,23 +20,44 @@
 // };
 
 
-const moveZeroes = (nums) => {
-    let i = 0;
-    let zero = 0;
+// const moveZeroes = (nums) => {
+//     let i = 0;
+//     let zero = 0;
 
-    while (i < nums.length) {
-        if (nums[i] === 0 && nums[zero] !== 0) {
-            nums[zero] = i;
+//     while (i < nums.length) {
+//         if (nums[i] === 0 && nums[zero] !== 0) {
+//             nums[zero] = i;
+//         }
+
+//         if (nums[i] !== 0 && zero < i) {
+//             nums[zero] = nums[i];
+//             nums[i] = 0;
+//         }
+
+//         i++
+//     }
+//     return nums;
+// }
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+    let pointer = 0;
+    if (nums.length <= 1) {
+        return nums;
+    }
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[pointer] = nums[i];
+            if (i !== pointer) {
+                nums[i] = 0;
+            }
+            pointer++;
         }
-
-        if (nums[i] !== 0 && zero < i) {
-            nums[zero] = nums[i];
-            nums[i] = 0;
-        }
-
-        i++
     }
     return nums;
-}
+};
 nums = [0, 1, 0, 3, 12]
 console.log(moveZeroes(nums));
